394ddce1cefa777771dc764cfdd3b71507e8bdd1
1 <aa>
2022-05-30 23:06:06 +0300

    sort

--- python/Sort/zoo/Zoo.py
+++ python/Sort/zoo/Zoo.py
@@ -6,6 +6,10 @@ if __name__ == '__main__':
     animals_male = animals.loc[(animals["gender"] == "male"), ["type"]].drop_duplicates()
     animals_female = animals.loc[(animals["gender"] == "female"), ["type"]].drop_duplicates()
     anim_pair = pd.merge(animals_male, animals_female, on="type", how='inner')
-    print(*[x[0] for x in anim_pair.reindex(anim_pair.type.str.len().sort_values().index).values], sep='\n')
+    if anim_pair.empty:
+        print(0)
+    else:
+        print(*[x[0] for x in anim_pair.reindex(anim_pair.type.str.len().sort_values().index).values], sep='\n')
+    
     animals_amount = animals["type"].value_counts().rename_axis("type").reset_index(name='Amount')
     print(*[f'{x[0]} - {x[1]}' for x in animals_amount.values], sep='\n')
